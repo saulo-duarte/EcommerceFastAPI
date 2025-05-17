27d8436272794084ef6bc337df4e8c20d3a1c97b
saulo-duarte <saulolduarte@outlook.com>
2025-05-17 15:10:13 -0300

    feat: atualiza __init__.py para incluir modelos de OrderStatus, Payment, Shipment e Review

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,9 +2,12 @@ from .address import Address
 from .cart import Cart
 from .cart_item import CartItem
 from .category import Category
-from .order import Order
+from .order import Order, OrderStatus
 from .order_item import OrderItem
+from .payment import Payment, PaymentMethod, PaymentStatus
 from .product import Product
+from .review import Review
+from .shipment import Shipment, ShipmentStatus
 from .user import User
 
 __all__ = [
@@ -16,4 +19,11 @@ __all__ = [
     "CartItem",
     "Order",
     "OrderItem",
+    "OrderStatus",
+    "Payment",
+    "PaymentStatus",
+    "PaymentMethod",
+    "Shipment",
+    "ShipmentStatus",
+    "Review",
 ]
