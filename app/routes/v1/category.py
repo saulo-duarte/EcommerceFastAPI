8e7f3068b092bccfb06c39597f1221741144770f
saulo-duarte <saulolduarte@outlook.com>
2025-05-28 09:48:36 -0300

    feat: add product categories CRUD operations

---
+++ app/routes/v1/category.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.session import get_async_db
+from app.schema.category import CategoryCreate, CategoryRead, CategoryUpdate
+from app.services.category import CategoryService
+
+router = APIRouter(prefix="/category", tags=["category"])
+
+@router.post("/", response_model=CategoryRead)
+async def create_category(
+    category_data: CategoryCreate,
+    db: AsyncSession = Depends(get_async_db),
+):
+    try:
+        category_service = CategoryService(db)
+        category = await category_service.create_category(category_data)
+        return category
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.get("/", response_model=list[CategoryRead])
+async def list_categories(db: AsyncSession = Depends(get_async_db)):
+    category_service = CategoryService(db)
+    categories = await category_service.list_categories()
+    return categories
+
+@router.get("/{category_id}", response_model=CategoryRead)
+async def get_category(
+    category_id: str,
+    db: AsyncSession = Depends(get_async_db),
+):
+    try:
+        category_service = CategoryService(db)
+        category = await category_service.get_category_by_id(category_id)
+        return category
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+@router.put("/{category_id}", response_model=CategoryRead)
+async def update_category(
+    category_id: str,
+    category_data: CategoryUpdate,
+    db: AsyncSession = Depends(get_async_db),
+):
+    try:
+        category_service = CategoryService(db)
+        updated_category = await category_service.update_category(category_id, category_data)
+        return updated_category
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+@router.delete("/{category_id}")
+async def delete_category(
+    category_id: str,
+    db: AsyncSession = Depends(get_async_db),
+):
+    try:
+        category_service = CategoryService(db)
+        await category_service.delete_category(category_id)
+        return {"detail": "Category deleted successfully"}
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
---
+++ app/services/category.py
@@ -0,0 +1,53 @@
+from typing import List
+from uuid import UUID
+
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.repository.category import CategoryRepository
+from app.schema.category import CategoryCreate, CategoryRead, CategoryUpdate
+
+
+class CategoryService:
+    def __init__(self, db: AsyncSession) -> None:
+        self.db = db
+        self.category_repository = CategoryRepository(db)
+
+    async def create_category(self, category_data: CategoryCreate) -> CategoryRead:
+        try:
+            new_category = await self.category_repository.create_category(category_data)
+            await self.db.commit()
+            await self.db.refresh(new_category)
+
+            return CategoryRead.model_validate(new_category, from_attributes=True)
+
+        except IntegrityError as e:
+            await self.db.rollback()
+            raise ValueError("Category creation failed due to integrity error") from e
+
+    async def get_category_by_id(self, category_id: UUID) -> CategoryRead:
+        category = await self.category_repository.get_by_id(category_id)
+        if not category:
+            raise ValueError("Category not found")
+        return CategoryRead.model_validate(category, from_attributes=True)
+
+    async def list_categories(self) -> List[CategoryRead]:
+        categories = await self.category_repository.list_categories()
+        return [CategoryRead.model_validate(category, from_attributes=True) for category in categories]
+
+    async def update_category(self, category_id: UUID, category_data: CategoryUpdate) -> CategoryRead:
+        category = await self.category_repository.get_by_id(category_id)
+        if not category:
+            raise ValueError("Category not found")
+
+        updated_category = await self.category_repository.update(category, category_data)
+        return CategoryRead.model_validate(updated_category, from_attributes=True)
+
+    async def delete_category(self, category_id: UUID) -> None:
+        category = await self.category_repository.get_by_id(category_id)
+        if not category:
+            raise ValueError("Category not found")
+
+        await self.category_repository.delete(category)
+        await self.db.commit()
+        return {"detail": "Category deleted successfully"}
